680b268207b2fda19535b5fbbdd8c4dd0103a050
lviit <lauriviitala3000@gmail.com>
2023-01-24 23:07:10 +0100

    google fonts

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -9,7 +9,15 @@ export const meta: MetaFunction = () => ({
 });
 
 export function links() {
-  return [{ rel: "stylesheet", href: styles }];
+  return [
+    { rel: "stylesheet", href: styles },
+    { rel: "preconnect", href: "https://fonts.googleapis.com" },
+    { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "true" },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;600;700&display=swap",
+    },
+  ];
 }
 
 export default function App() {
